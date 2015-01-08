0cb6d2c5a48ccfe7b30cbdce8b8e7989e2bbfbfa
fellon83 <f.amoruso14@studenti.uniba.it>
2015-01-08 17:57:39 +0100

    Soluzione appelli 2013 - 2014 R
    Update: aggiunta soluzione 14-02-2014

---
+++ soluzioni r/svolgimento_LAB_14.02.14.R
@@ -0,0 +1,37 @@
+rm(list=ls())
+#importo il dataset
+
+library(mlbench)
+data("PimaIndiansDiabetes2")
+#"rinomino" per una facilità di lettura
+data.frame <- PimaIndiansDiabetes2
+
+#rimuovogli i valori na visto che dovremo svolgere un knn, sono presenti nelle colonne 2:6
+
+for (c in 2:6) {
+	valore.medio <- round(mean(data.frame[,c], na.rm=T))
+	data.frame[is.na(data.frame[,c]),c] <- valore.medio
+
+}
+
+
+
+#suddivisione in training e test in maniera casuale(gli indici vengono
+#ordinati per una migliore lettura
+index.random <- sort(sample(nrow(data.frame), size=nrow(data.frame)*0.75, replace=F))
+
+#suddivisione in 75-25
+data.train <- data.frame[index.random, ]
+data.test <- data.frame[-index.random, ]
+
+#verifico se l'ultima colonna è Factor 
+str(data.frame)
+
+#effettuo il knn
+pred <- knn(data.train[,-9], data.test[,-9], data.train[,9], k=3, prob=F)
+
+#creo una matrice di confusione
+conf.matrix <- table (true = data.test[,9], pred)
+
+#verifico l'accuratezza
+classAgreement(conf.matrix)$diag
